1f841bf4ed2e2a5f2c40546aa9b902d5d307a78c
Shaeeg <m.shaeeg@gmail.com>
2026-05-04 18:05:04 +0000

    Add inline doctests to integer_validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,11 +6,30 @@ class BaseGeometry:
     """A class representing base geometry."""
 
     def area(self):
-        """Raise an Exception indicating area() is not implemented."""
+        """Raise an Exception indicating area() is not implemented.
+
+        >>> bg = BaseGeometry()
+        >>> bg.area()
+        Traceback (most recent call last):
+        Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
+        """Validate that value is a positive integer.
+
+        >>> bg = BaseGeometry()
+        >>> bg.integer_validator("my_int", 12)
+        >>> bg.integer_validator("name", "John")
+        Traceback (most recent call last):
+        TypeError: name must be an integer
+        >>> bg.integer_validator("age", 0)
+        Traceback (most recent call last):
+        ValueError: age must be greater than 0
+        >>> bg.integer_validator("distance", -4)
+        Traceback (most recent call last):
+        ValueError: distance must be greater than 0
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
