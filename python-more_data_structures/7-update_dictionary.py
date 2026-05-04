23985546e98504b3c773ed66a2ead66649a18e77
Shaeeg <m.shaeeg@gmail.com>
2026-05-04 21:17:40 +0000

    Add update_dictionary function

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that replaces or adds key/value in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add a key/value pair in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
