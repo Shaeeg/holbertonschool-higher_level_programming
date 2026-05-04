ed9e07467e1d9c8f5a6a7d555f714f8d3ade9d05
Shaeeg <m.shaeeg@gmail.com>
2026-05-04 21:18:24 +0000

    Add multiply_by_2 function

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that returns a new dictionary with all values multiplied by 2."""
+
+
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multiplied by 2."""
+    return {k: v * 2 for k, v in a_dictionary.items()}
