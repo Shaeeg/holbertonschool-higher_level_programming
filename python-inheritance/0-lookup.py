a600f0c0a65b5eac529bd4b2aa2065e2de62e078
Shaeeg <m.shaeeg@gmail.com>
2026-05-04 17:57:30 +0000

    Fix pycodestyle line too long in 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that returns the list of available attributes and methods of an object."""
+"""Module for looking up object attributes and methods."""
 
 
 def lookup(obj):
