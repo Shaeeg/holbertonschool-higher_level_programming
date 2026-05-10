1bf8b64bd722b5a9caddaea4335fd999c9745553
“Shaeeg” <m.shaeeg@gmail.com>
2026-05-10 21:08:11 +0000

    Count TV shows per genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- Genres with show counts (exclude unused genres), one SELECT
-SELECT tv_genres.name AS genre, COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
+-- Genres with show counts; genres with no shows omitted; one SELECT
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id, tv_genres.name
-ORDER BY number_of_shows DESC;
+ORDER BY 2 DESC;
