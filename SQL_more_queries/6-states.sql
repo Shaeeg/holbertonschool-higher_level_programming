57f5eca8cf376f015b850caaf9023f22da627831
“Shaeeg” <m.shaeeg@gmail.com>
2026-05-10 21:02:57 +0000

    Add hbtn_0d_usa database and states table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Database hbtn_0d_usa and table states (id auto-increment PK, name NOT NULL)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id)
+);
