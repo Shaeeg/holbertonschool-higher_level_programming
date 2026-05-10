6a48d99095f3160fcc549588fae873a6d355a842
“Shaeeg” <m.shaeeg@gmail.com>
2026-05-10 21:06:02 +0000

    List all shows with genre id including NULL

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- All shows with genre_id or NULL if none, one SELECT
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
